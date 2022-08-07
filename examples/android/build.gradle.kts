0a579a6f09725d839ecad292570b6b07b9125dce
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2022-08-07 15:44:27 +0200

    Update example dependencies

--- examples/android/build.gradle.kts
+++ examples/android/build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
     implementation(project(":cokoin"))
     implementation(project(":cokoin-android-viewmodel"))
     implementation(project(":cokoin-android-navigation"))
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.core:core-ktx:1.8.0")
     implementation(compose.material)
 }
